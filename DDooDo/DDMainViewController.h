f7fd9f714415208ba8ccf78aea3f2f61be2e71f3
Gichan Nam <admin@designplusd.com>
2012-03-08 12:43:58 +0900

    UI 작업 시작...

---
+++ DDooDo/DDMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  DDMainViewController.h
+//  DDooDo
+//
+//  Created by 기찬 남 on 12. 3. 8..
+//  Copyright (c) 2012년 nobird01@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDMainViewController : UIViewController
+
+@end
---
+++ DDooDo/DDMainViewController.m
@@ -0,0 +1,43 @@
+//
+//  DDMainViewController.m
+//  DDooDo
+//
+//  Created by 기찬 남 on 12. 3. 8..
+//  Copyright (c) 2012년 nobird01@gmail.com. All rights reserved.
+//
+
+#import "DDMainViewController.h"
+
+@interface DDMainViewController ()
+
+@end
+
+@implementation DDMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
