0e1fa495b16537127663267f27140980abd85889
leedongins <admin@designplusd.com>
2012-03-08 17:57:54 +0900

    해피니스 추

---
+++ Projects/Happiness/Happiness/HappinessViewController.h
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by 동인 이 on 12. 3. 8..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController 
+
+@property (nonatomic) int happiness; // 0 is sad; 100 is very happy
+@end
---
+++ Projects/Happiness/Happiness/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Happiness
+//
+//  Created by 동인 이 on 12. 3. 8..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HappinessAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HappinessAppDelegate class]));
+    }
+}
